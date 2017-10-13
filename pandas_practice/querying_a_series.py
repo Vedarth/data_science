a25d88ffff1285c3c086ee81191341e34d878090
Vedarth <vedarth.sharma@gmail.com>
2017-10-13 06:37:55 +0530

    Update querying in series

--- pandas_practice/querying_a_series.py
+++ pandas_practice/querying_a_series.py
@@ -6,6 +6,30 @@ s = pd.Series(sports)
 s
 
 s.iloc[3]
+
 s.loc['Golf']
+
 s[3]
+
 s['Golf']
+
+sports = {99: 'Bhutan',
+          100: 'Scotland',
+          101: 'Japan',
+          102: 'South Korea'}
+s = pd.Series(sports)
+
+s.iloc[99]
+
+s = pd.Series([100.00, 120.00, 101.00, 3.00])
+s
+
+total = 0
+for item in s:
+    total+=item
+print(total)
+
+total = 0
+for item in s:
+    total+=item
+print(total)
