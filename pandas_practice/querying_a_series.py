72043b382498ec51840d1bc8756f068096205df6
Vedarth <vedarth.sharma@gmail.com>
2017-10-13 06:35:42 +0530

    Add querying in series

---
+++ pandas_practice/querying_a_series.py
@@ -0,0 +1,11 @@
+sports = {'Archery': 'Bhutan',
+          'Golf': 'Scotland',
+          'Sumo': 'Japan',
+          'Taekwondo': 'South Korea'}
+s = pd.Series(sports)
+s
+
+s.iloc[3]
+s.loc['Golf']
+s[3]
+s['Golf']
