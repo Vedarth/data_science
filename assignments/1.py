357f1b9935776858a52b559d0e5053ec47f649d4
Vedarth <vedarth.sharma@gmail.com>
2017-10-19 00:24:03 +0530

    Correction in 7

--- assignments/1.py
+++ assignments/1.py
@@ -78,19 +78,13 @@ def answer_six():
 answer_six()
 
 def answer_seven():
-    max_abs_change = 0
-    abs_changes = list()
-    abs_change = 0
-    for i in range(3193):
-        abs_change = 0
-        abs_changes = []
-        for j in range(6):
-            abs_changes.append(census_df.iloc[i][9+j])
-            abs_change = abs(max(abs_changes) - min(abs_changes))
-        if max_abs_change < abs_change:
-            max_abs_change = abs_change
-            st = census_df.iloc[i].iloc[6]
-    return st
+    counties_df = census_df[census_df['SUMLEV'] == 50]
+    counties_df['pop_change'] = abs(counties_df['POPESTIMATE2015'] - counties_df['POPESTIMATE2014'])+abs(counties_df['POPESTIMATE2014'] - counties_df['POPESTIMATE2013'])+abs(counties_df['POPESTIMATE2013'] - counties_df['POPESTIMATE2012'])+abs(counties_df['POPESTIMATE2012'] - counties_df['POPESTIMATE2011'])+abs(counties_df['POPESTIMATE2011'] - counties_df['POPESTIMATE2010'])
+    a = max(counties_df['pop_change'])
+    ans = counties_df['CTYNAME'][counties_df['pop_change']==a].tolist()
+    return ans[0]
+
+
 answer_seven()
 
 def answer_eight():
